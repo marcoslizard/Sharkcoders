2302b18e56b1848513a3e77660424f9cc7470b4f
marcoslizard <Marcodomi101@gmail.com>
2025-11-08 12:51:08 +0000

    Add files via upload

---
+++ blogproject/blogapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+    def post(self,request , *args , **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
+
+            return redirect('Post_detail', pk=self.object.pk)
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url= reverse_lazy('post_list')
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+# Create your views here.
+
---
+++ todolist/todoapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {"task": tasks, "form": form}
+    return render(request, "todo/index.html",context)
+
+def deleteTask(request , pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return redirect("/")
+
+def completeTask(request, pk):
+    task = Task.objects.get(id=pk)
+    task.completed = not task.completed
+    task.save()
+    return redirect("/")
+
+
+
+
+
